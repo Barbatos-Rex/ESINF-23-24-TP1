b9a9842f361fe3d0385d5e899a03c910a19639ef
Andre <1190387@isep.ipp.pt>
2023-09-29 13:18:11 +0100

    Added ex8 to repo

--- src/main/java/pt/ipp/isep/esinf/functionality/EletricChargersPerCountry.java
+++ src/main/java/pt/ipp/isep/esinf/functionality/EletricChargersPerCountry.java
@@ -2,6 +2,7 @@ package pt.ipp.isep.esinf.functionality;
 
 import pt.ipp.isep.esinf.algorithm.TableMaximumMinimumDistanceBetweenChargersAlgorithm;
 import pt.ipp.isep.esinf.data.DataBitChargers;
+import pt.ipp.isep.esinf.structs.ChargingCapacity;
 import pt.ipp.isep.esinf.structs.EletricStationsByKw;
 import pt.ipp.isep.esinf.structs.MinimumAutonomy;
 
@@ -38,7 +39,7 @@ public class EletricChargersPerCountry {
     }
 
     public Set<MinimumAutonomy> maximumMinimumDistanceBetweenChargingStationsByCountry() {
-        Map<String,Set<DataBitChargers>> chargersByCountry = mapToChargersByCountry();
+        Map<String, Set<DataBitChargers>> chargersByCountry = mapToChargersByCountry();
         Set<MinimumAutonomy> result = new TreeSet<>(new MinimumAutonomy.MinimumAutonomyComparatorByAutonomyDecreasingCountryAlphabetically(new TableMaximumMinimumDistanceBetweenChargersAlgorithm()));
         return result;
     }
@@ -53,4 +54,56 @@ public class EletricChargersPerCountry {
         }
         return result;
     }
+
+
+    public enum Option {
+        CITY,
+        STATE;
+    }
+
+    public Set<ChargingCapacity> getNStatesWithHighestCapacity(Collection<String> cityStates, Option option, int n) {
+
+        switch (option) {
+            case CITY -> {
+                return getNStatesWithHighestCapacity(citiesToStates(cityStates), Option.STATE, n);
+            }
+            case STATE -> {
+                Set<DataBitChargers> stateData = filterChargersByStates(cityStates);
+                Map<String, ChargingCapacity> capacity = new HashMap<>();
+                for (DataBitChargers sData : stateData) {
+                    if (capacity.containsKey(sData.getState())) {
+                        capacity.put(sData.getState(), new ChargingCapacity(sData.getState(), new HashSet<>(), 0));
+                    }
+                    capacity.get(sData.getState()).addEntry(sData);
+                }
+                return new TreeSet<>(capacity.values());
+            }
+            default -> throw new UnsupportedOperationException("That option is not implemented yet!");
+        }
+    }
+
+
+    private Set<DataBitChargers> filterChargersByStates(Collection<String> states) {
+        Set<DataBitChargers> result = new HashSet<>();
+        for (DataBitChargers bit : data) {
+            if (states.contains(bit.getState())) {
+                result.add(bit);
+            }
+        }
+        return result;
+    }
+
+
+    private Set<String> citiesToStates(Collection<String> cities) {
+
+        Set<String> result = new HashSet<>();
+        for (DataBitChargers bit : data) {
+            if (cities.contains(bit.getCity())) {
+                result.add(bit.getState());
+            }
+        }
+        return result;
+    }
+
+
 }
---
+++ src/main/java/pt/ipp/isep/esinf/structs/ChargingCapacity.java
@@ -0,0 +1,58 @@
+package pt.ipp.isep.esinf.structs;
+
+import pt.ipp.isep.esinf.data.DataBitChargers;
+
+import java.util.Objects;
+import java.util.Set;
+
+public class ChargingCapacity implements Comparable<ChargingCapacity> {
+    private String state;
+    private Set<String> cities;
+    private int chargingCapacity;
+
+    public ChargingCapacity(String state, Set<String> cities, int chargingCapacity) {
+        this.cities = cities;
+        this.chargingCapacity = chargingCapacity;
+        this.state = state;
+    }
+
+    public Set<String> getCities() {
+        return cities;
+    }
+
+    public int getChargingCapacity() {
+        return chargingCapacity;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChargingCapacity that = (ChargingCapacity) o;
+        return Objects.equals(state, that.state);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(state);
+    }
+
+
+    public void addEntry(DataBitChargers bit){
+        if (!bit.getState().equals(state)){
+            return;
+        }
+        chargingCapacity+=Integer.parseInt(bit.getkW());
+        cities.add(bit.getCity());
+    }
+
+
+    @Override
+    public int compareTo(ChargingCapacity o) {
+        return -Integer.compare(chargingCapacity,o.chargingCapacity);
+    }
+}
